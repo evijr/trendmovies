07a6731212cdc450ee85ffb838064945c98c4172
Eulalio Ibay Jr <97023382+evijr@users.noreply.github.com>
2025-05-12 03:49:11 +0800

    Update home.js

--- my-website/js/home.js
+++ my-website/js/home.js
@@ -1,4 +1,4 @@
-const API_KEY = 'a1e72fd93ed59f56e6332813b9f8dcae';
+const API_KEY = 'ac0eb5e1a740af26dfaae188c38ae208';
     const BASE_URL = 'https://api.themoviedb.org/3';
     const IMG_URL = 'https://image.tmdb.org/t/p/original';
     let currentItem;
